29adf2aac06ff8f3be1a7c146c4702eb6935c336
Subham Mani <manisubham09@gmail.com>
2024-10-08 20:11:23 +0530

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-
+import React from "react";
 import { LandingPageComponent } from "@/components/landing-page"; // Import the LandingPageComponent
 
 export default function Home() {
